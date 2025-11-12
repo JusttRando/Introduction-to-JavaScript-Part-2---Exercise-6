29c0227aee03912aa0d4f1ff6a19ea5363545d20
Rando <justtrando@gmail.com>
2025-11-12 12:19:55 +0200

    More to come

--- async_promises.js
+++ async_promises.js
@@ -2,31 +2,98 @@
 // - The Promise should resolve after 1.5 seconds
 // - If userId is positive, resolve with user data object
 // - If userId is negative or zero, reject with an error
-// - User data should include: id, name, email, and registrationDate// TODO: Create a function that uses template literals for HTML generation
+// - User data should include: id, name, email, and registrationDate
+
+const UserID = "0"
+
+function userInput(IDs)
+{
+
+    const ID = parseInt(IDs)
+
+    try
+    {
+        const StudInfo = {id:ID, name:"Antonio", email:"AntonioUno@gmail.com", registrationDate:"16/01/2023"}
+        
+        if ( Number.isInteger(ID) || ID >= 0|| ID !== 0)
+        {
+            console.log
+            (`
+                
+                              Student Info 
+
+                ⠀⠀⠀⠀⠀⢀⣤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⢤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⢀⡼⠋⠀⣀⠄⡂⠍⣀⣒⣒⠂⠀⠬⠤⠤⠬⠍⠉⠝⠲⣄⡀⠀⠀ID = ${StudInfo.id}
+                ⠀⠀⠀⢀⡾⠁⠀⠊⢔⠕⠈⣀⣀⡀⠈⠆⠀⠀⠀⡍⠁⠀⠁⢂⠀⠈⣷⠀⠀
+                ⠀⠀⣠⣾⠥⠀⠀⣠⢠⣞⣿⣿⣿⣉⠳⣄⠀⠀⣀⣤⣶⣶⣶⡄⠀⠀⣘⢦⡀ Name = ${StudInfo.name}
+                ⢀⡞⡍⣠⠞⢋⡛⠶⠤⣤⠴⠚⠀⠈⠙⠁⠀⠀⢹⡏⠁⠀⣀⣠⠤⢤⡕⠱⣷
+                ⠘⡇⠇⣯⠤⢾⡙⠲⢤⣀⡀⠤⠀⢲⡖⣂⣀⠀⠀⢙⣶⣄⠈⠉⣸⡄⠠⣠⡿  Email = ${StudInfo.email}
+                ⠀⠹⣜⡪⠀⠈⢷⣦⣬⣏⠉⠛⠲⣮⣧⣁⣀⣀⠶⠞⢁⣀⣨⢶⢿⣧⠉⡼⠁
+                ⠀⠀⠈⢷⡀⠀⠀⠳⣌⡟⠻⠷⣶⣧⣀⣀⣹⣉⣉⣿⣉⣉⣇⣼⣾⣿⠀⡇⠀ Date = ${StudInfo.registrationDate}
+                ⠀⠀⠀⠈⢳⡄⠀⠀⠘⠳⣄⡀⡼⠈⠉⠛⡿⠿⠿⡿⠿⣿⢿⣿⣿⡇⠀⡇⠀
+                ⠀⠀⠀⠀⠀⠙⢦⣕⠠⣒⠌⡙⠓⠶⠤⣤⣧⣀⣸⣇⣴⣧⠾⠾⠋⠀⠀⡇⠀Thank You !!!
+                ⠀⠀⠀⠀⠀⠀⠀⠈⠙⠶⣭⣒⠩⠖⢠⣤⠄⠀⠀⠀⠀⠀⠠⠔⠁⡰⠀⣧⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⢤⣀⣀⠉⠉⠀⠀⠀⠀⠀⠁⠀⣠⠏⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠒⠲⠶⠤⠴⠒⠚⠁
+
+                `)
+        }
+        else
+        {
+            throw new Error("Enter number ID please")
+        }
+    }
+    catch(e)
+    {
+        console.log(e)
+    }
+}
+
+userInput(UserID)
+
+// TODO: Create a function that uses template literals for HTML generation
+
+
+
 // TODO: Create a Promise that simulates fetching user posts
 // - Should resolve after 1 second
 // - Return an array of post objects
 // - Each post should have: id, title, content, and userId
 // - If userId doesn't exist, reject with error
+
+
+
 // TODO: Create a function that chains multiple Promises together
 // - First fetch user data
 // - Then fetch their posts
 // - Combine the data into a single object
 // - Handle any errors that occur in the chain
+
+
+
 // TODO: Convert the above Promise chain to use async/await
 // - Use try/catch for error handling
 // - Log each step of the process
 // - Return combined user and posts data
+
+
+
 // TODO: Create a function that fetches multiple users in parallel
 // - Take an array of userIds
 // - Fetch all users simultaneously using Promise.all
 // - Handle errors for individual user fetches
 // - Return array of successfully fetched users
+
+
+
 // TODO: Create a function that fetches users and their posts in parallel
 // - Fetch user data for multiple users
 // - Once user data is received, fetch all their posts in parallel
 // - Combine user and posts data
 // - Handle errors appropriately
+
+
+
 // TODO: Test success cases
 // - Test single user fetch
 // - Test multiple user fetch
